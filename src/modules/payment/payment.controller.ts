afe7d2b0b784d378e0bfa5369841ec9b9c273f3c
LanNguyen <lan.nguyen@stunited.vn>
2025-04-11 15:48:39 +0700

    Handle VNPay return and redirect to frontend

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Post, Get, Body, Query, Param, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Query,
+  Param,
+  UseGuards,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
-import { Payment, PaymentMethod } from '../../entities/payment.entity';
+import { Payment, PaymentMethod, PaymentStatus } from '../../entities/payment.entity';
 import { Request } from 'express';
 import { User } from '../../entities/user.entity';
 import { PageOptionsDto } from 'src/common/dtos';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Response } from 'express';
+import { Public } from 'src/auth/Public';
 
 interface RequestWithUser extends Request {
   user: User;
@@ -21,9 +33,26 @@ export class PaymentController {
     @Body() createPaymentDto: CreatePaymentDto,
     @Req() req: RequestWithUser,
   ): Promise<Payment & { paymentUrl: string }> {
-    return this.paymentService.createPayment(createPaymentDto, req.user.id.toString());
+    return this.paymentService.createPayment(
+      createPaymentDto,
+      req.user.id.toString(),
+    );
   }
 
+  @Public()
+  @Get('vnpay-return')
+async handleVnpayReturn(@Query() query: any, @Res() res: Response) {
+  const result = await this.paymentService.handlePaymentCallback(PaymentMethod.VNPAY, query);
+
+  if (result.status === PaymentStatus.COMPLETED) {
+
+    return res.redirect(`${process.env.FRONTEND_URL}/payment/success?orderId=${result.id}`);
+  } else {
+    return res.redirect(`${process.env.FRONTEND_URL}/payment/failed`);
+  }
+}
+
+  
   @Get()
   async findAll(@Query() pageOptionsDto: PageOptionsDto) {
     return this.paymentService.findAll(pageOptionsDto);
@@ -34,7 +63,10 @@ export class PaymentController {
     @Query() pageOptionsDto: PageOptionsDto,
     @Req() req: RequestWithUser,
   ) {
-    return this.paymentService.findByUser(req.user.id.toString(), pageOptionsDto);
+    return this.paymentService.findByUser(
+      req.user.id.toString(),
+      pageOptionsDto,
+    );
   }
 
   @Get(':id')
@@ -49,4 +81,4 @@ export class PaymentController {
   ) {
     return this.paymentService.handlePaymentCallback(method, params);
   }
-} 
+}
